0a0df8a62f56978be90f533cbdfd036c0f88fa51
Andrew Petersen <kirbysayshi@gmail.com>
2014-08-12 00:28:49 -0400

    clustering, drag drop, palettes!

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+var Clusterer = require('./lib/clusterer');
+var converge = require('./lib/converge');
+var palettes = require('./lib/palettes');
+
 var imgInput = document.getElementById('img-input');
 var cvsOutputRgb = document.getElementById('cvs-n-color-rgb');
 var ctxOutputRgb = cvsOutputRgb.getContext('2d');
@@ -30,13 +34,30 @@ document.addEventListener('drop', function(e) {
   reader.readAsDataURL(files[0]);
   reader.addEventListener('loadend', function() {
     imgInput.src = reader.result;
-    imgInput.addEventListener('load', function(e) {
+    imgInput.addEventListener('load', function load(e) {
+      imgInput.removeEventListener('load', load);
       console.log('input display ready');
 
       cvsOutputRgb.width = imgInput.width;
       cvsOutputRgb.height = imgInput.height;
 
       ctxOutputRgb.drawImage(imgInput, 0, 0);
+
+      var imgData = ctxOutputRgb.getImageData(0, 0, imgInput.width, imgInput.height);
+      var clusterCount = 4; // TODO: this should come from palette
+      var clusterData = Clusterer.init(imgData.data, clusterCount, 4);
+
+      var async = false;
+      converge(clusterData, async, function progress() {},
+        function(err, clusterData, convergeCount) {
+          console.log('converged in', convergeCount);
+        })
+
+      // Make a blank destination image data.
+      var palette = palettes.gameboy.slice(0);
+      var outputImageData = ctxOutputRgb.createImageData(imgData);
+      Clusterer.applyPaletteToImageData(clusterData, palette, outputImageData);
+      ctxOutputRgb.putImageData(outputImageData, 0, 0);
     })
   })
 
