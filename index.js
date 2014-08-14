492d295768c7e1a9e967c2452fc72bc84e61009a
Andrew Petersen <kirbysayshi@gmail.com>
2014-08-14 01:55:11 -0400

    allow palettes via dom options

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ var converge = require('./lib/converge');
 var palettes = require('./lib/palettes');
 
 var cvsOutputRgb = document.getElementById('cvs-n-color-rgb');
-var ctxOutputRgb = cvsOutputRgb.getContext('2d');
+
+function current() {
+
+  var q = document.querySelector.bind(document);
+
+  return {
+    palette: palettes[q('[name=options-palettes]:checked').value],
+    image: q('#img-input'),
+    dstCvs: q('#cvs-n-color-rgb'),
+    async: q('#options-async').checked
+  }
+}
 
 document.addEventListener('dragenter', function(e) {
   document.body.classList.add('drag-valid');
@@ -32,46 +43,66 @@ document.addEventListener('drop', function(e) {
   var reader = new FileReader();
   reader.readAsDataURL(files[0]);
   reader.addEventListener('loadend', function() {
-    var imgInput = document.createElement('img');
-    imgInput.src = reader.result;
-    imgInput.addEventListener('load', function load(e) {
-      imgInput.removeEventListener('load', load);
+
+    var c = current();
+    c.image.src = reader.result;
+    c.image.addEventListener('load', function load(e) {
+      c.image.removeEventListener('load', load);
       console.log('input display ready');
-
-      cvsOutputRgb.width = imgInput.width;
-      cvsOutputRgb.height = imgInput.height;
-
-      ctxOutputRgb.drawImage(imgInput, 0, 0);
-
-      var imgData = ctxOutputRgb.getImageData(0, 0, imgInput.width, imgInput.height);
-      var clusterCount = 4; // TODO: this should come from palette
-      var clusterData = Clusterer.init(imgData.data, clusterCount, 4);
-
-      var async = false;
-      var ASYNC_AREA_LIMIT = 120000;
-
-      if (imgData.width * imgData.height > ASYNC_AREA_LIMIT) {
-        async = true;
-      }
-
-      var convergeStart = window.performance.now();
-      var palette = palettes.gameboy.slice(0);
-      var outputImageData = ctxOutputRgb.createImageData(imgData);
-
-      converge(clusterData, async,
-        function progress(clusterData, convergeCount, pixelsMoved) {
-          console.log('converge', convergeCount, async == true ? 'ASYNC' : 'SYNC', pixelsMoved);
-          Clusterer.applyPaletteToImageData(clusterData, palette, outputImageData);
-          ctxOutputRgb.putImageData(outputImageData, 0, 0);
-        },
-        function(err, clusterData, convergeCount) {
-          console.log('converged in', convergeCount, (window.performance.now() - convergeStart) + 'ms');
-          Clusterer.applyPaletteToImageData(clusterData, palette, outputImageData);
-          ctxOutputRgb.putImageData(outputImageData, 0, 0);
-        })
+      redraw(c.image, c.dstCvs, c.palette);
     })
   })
 
   e.preventDefault();
   e.stopPropagation();
 }, false)
+
+// Listen to options
+document.addEventListener('change', function(e) {
+  var c = current();
+
+  if (!c.image.src) return;
+
+  redraw(c.image, c.dstCvs, c.palette);
+})
+
+function redraw(srcImg, dstCvs, palette, opt_cb) {
+  var dstCtx = dstCvs.getContext('2d');
+
+  // apply image to canvas
+  dstCvs.width = srcImg.width;
+  dstCvs.height = srcImg.height;
+  dstCtx.drawImage(srcImg, 0, 0);
+
+  // Init clusterer.
+  var srcData = dstCtx.getImageData(0, 0, srcImg.width, srcImg.height);
+  var dataFactor = 4; // assume rgba for now.
+  var clusterCount = palette.length / dataFactor; // Assume rgba for now.
+  var clusterData = Clusterer.init(srcData.data, clusterCount, dataFactor);
+
+  // Init output data.
+  var outputImageData = dstCtx.createImageData(srcData);
+
+  var ASYNC_AREA_LIMIT = 120000;
+
+  var srcArea = srcData.width * srcData.height;
+  var async = srcArea > ASYNC_AREA_LIMIT
+    ? true
+    : false;
+
+  var convergeStart = window.performance.now();
+  converge(clusterData, async, progress, complete);
+
+  function progress(clusterData, convergeCount, pixelsMoved) {
+    console.log('converge', convergeCount, async == true ? 'ASYNC' : 'SYNC', pixelsMoved);
+    Clusterer.applyPaletteToImageData(clusterData, palette, outputImageData);
+    dstCtx.putImageData(outputImageData, 0, 0);
+  }
+
+  function complete(err, clusterData, convergeCount) {
+    console.log('converged in', convergeCount, (window.performance.now() - convergeStart) + 'ms');
+    Clusterer.applyPaletteToImageData(clusterData, palette, outputImageData);
+    dstCtx.putImageData(outputImageData, 0, 0);
+    if (opt_cb) opt_cb.apply(null, arguments);
+  }
+}
