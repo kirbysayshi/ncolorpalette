098a91777feaba3a3aec94a225ab3ddc5ef497db
Andrew Petersen <kirbysayshi@gmail.com>
2014-08-15 02:56:56 -0400

    bundle

--- bundle.js
+++ bundle.js
@@ -1,4 +1,8 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+
+// Polyfill auto attached self to canvas proto if needed.
+require('./vendor/canvas-toBlob');
+
 var Clusterer = require('./lib/clusterer');
 var converge = require('./lib/converge');
 var palettes = require('./lib/palettes');
@@ -21,6 +25,12 @@ function current() {
     async: q('#options-async').checked,
     dstImg: q('#img-output')
   }
+}
+
+function now() {
+  return window.performance
+    ? window.performance.now()
+    : Date.now();
 }
 
 document.addEventListener('dragenter', function(e) {
@@ -110,7 +120,7 @@ function redraw(opts, opt_cb) {
   // Init output data.
   var outputImageData = dstCtx.createImageData(srcData);
 
-  var convergeStart = window.performance.now();
+  var convergeStart = now();
   currentConvergence = converge(clusterData, async, progress, complete);
 
   function progress(clusterData, convergeCount, pixelsMoved) {
@@ -121,7 +131,7 @@ function redraw(opts, opt_cb) {
 
   function complete(err, clusterData, convergeCount) {
 
-    var time = window.performance.now() - convergeStart;
+    var time = now() - convergeStart;
 
     document.querySelector('#output-stats').textContent =
       '(' + convergeCount + ' iterations, ' + time.toFixed(2) + 'ms)';
@@ -129,7 +139,15 @@ function redraw(opts, opt_cb) {
     console.log('converged in', convergeCount, time + 'ms');
     Clusterer.applyPaletteToImageData(clusterData, palette, outputImageData);
     dstCtx.putImageData(outputImageData, 0, 0);
-    dstImg.src = dstCvs.toDataURL();
+
+    dstCvs.toBlob(function(blob) {
+      var url = URL.createObjectURL(blob);
+      dstImg.addEventListener('load', function onload() {
+        URL.revokeObjectURL(url);
+        dstImg.removeEventListener('load', onload);
+      })
+      dstImg.src = url;
+    })
 
     // Hide canvas, show image to allow for dragging out of the browser.
     setTimeout(function() {
@@ -140,7 +158,7 @@ function redraw(opts, opt_cb) {
     })
   }
 }
-},{"./lib/clusterer":3,"./lib/converge":4,"./lib/palettes":5}],2:[function(require,module,exports){
+},{"./lib/clusterer":3,"./lib/converge":4,"./lib/palettes":5,"./vendor/canvas-toBlob":6}],2:[function(require,module,exports){
 function AllocatedArray(maxLength, opt_type) {
   this._length = 0;
   this.data = new (opt_type || Uint32Array)(maxLength);
@@ -432,4 +450,130 @@ exports['muted-kirby'] = [
   251, 217, 216, 255/*,
   255, 255, 255, 255*/
 ]
+},{}],6:[function(require,module,exports){
+/* canvas-toBlob.js
+ * A canvas.toBlob() implementation.
+ * 2013-12-27
+ * 
+ * By Eli Grey, http://eligrey.com and Devin Samarin, https://github.com/eboyjr
+ * License: X11/MIT
+ *   See https://github.com/eligrey/canvas-toBlob.js/blob/master/LICENSE.md
+ */
+
+/*global self */
+/*jslint bitwise: true, regexp: true, confusion: true, es5: true, vars: true, white: true,
+  plusplus: true */
+
+/*! @source http://purl.eligrey.com/github/canvas-toBlob.js/blob/master/canvas-toBlob.js */
+
+(function(view) {
+"use strict";
+var
+    Uint8Array = view.Uint8Array
+  , HTMLCanvasElement = view.HTMLCanvasElement
+  , canvas_proto = HTMLCanvasElement && HTMLCanvasElement.prototype
+  , is_base64_regex = /\s*;\s*base64\s*(?:;|$)/i
+  , to_data_url = "toDataURL"
+  , base64_ranks
+  , decode_base64 = function(base64) {
+    var
+        len = base64.length
+      , buffer = new Uint8Array(len / 4 * 3 | 0)
+      , i = 0
+      , outptr = 0
+      , last = [0, 0]
+      , state = 0
+      , save = 0
+      , rank
+      , code
+      , undef
+    ;
+    while (len--) {
+      code = base64.charCodeAt(i++);
+      rank = base64_ranks[code-43];
+      if (rank !== 255 && rank !== undef) {
+        last[1] = last[0];
+        last[0] = code;
+        save = (save << 6) | rank;
+        state++;
+        if (state === 4) {
+          buffer[outptr++] = save >>> 16;
+          if (last[1] !== 61 /* padding character */) {
+            buffer[outptr++] = save >>> 8;
+          }
+          if (last[0] !== 61 /* padding character */) {
+            buffer[outptr++] = save;
+          }
+          state = 0;
+        }
+      }
+    }
+    // 2/3 chance there's going to be some null bytes at the end, but that
+    // doesn't really matter with most image formats.
+    // If it somehow matters for you, truncate the buffer up outptr.
+    return buffer;
+  }
+;
+if (Uint8Array) {
+  base64_ranks = new Uint8Array([
+      62, -1, -1, -1, 63, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1
+    , -1, -1,  0, -1, -1, -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9
+    , 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25
+    , -1, -1, -1, -1, -1, -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35
+    , 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51
+  ]);
+}
+if (HTMLCanvasElement && !canvas_proto.toBlob) {
+  canvas_proto.toBlob = function(callback, type /*, ...args*/) {
+      if (!type) {
+      type = "image/png";
+    } if (this.mozGetAsFile) {
+      callback(this.mozGetAsFile("canvas", type));
+      return;
+    } if (this.msToBlob && /^\s*image\/png\s*(?:$|;)/i.test(type)) {
+      callback(this.msToBlob());
+      return;
+    }
+
+    var
+        args = Array.prototype.slice.call(arguments, 1)
+      , dataURI = this[to_data_url].apply(this, args)
+      , header_end = dataURI.indexOf(",")
+      , data = dataURI.substring(header_end + 1)
+      , is_base64 = is_base64_regex.test(dataURI.substring(0, header_end))
+      , blob
+    ;
+    if (Blob.fake) {
+      // no reason to decode a data: URI that's just going to become a data URI again
+      blob = new Blob
+      if (is_base64) {
+        blob.encoding = "base64";
+      } else {
+        blob.encoding = "URI";
+      }
+      blob.data = data;
+      blob.size = data.length;
+    } else if (Uint8Array) {
+      if (is_base64) {
+        blob = new Blob([decode_base64(data)], {type: type});
+      } else {
+        blob = new Blob([decodeURIComponent(data)], {type: type});
+      }
+    }
+    callback(blob);
+  };
+
+  if (canvas_proto.toDataURLHD) {
+    canvas_proto.toBlobHD = function() {
+      to_data_url = "toDataURLHD";
+      var blob = this.toBlob();
+      to_data_url = "toDataURL";
+      return blob;
+    }
+  } else {
+    canvas_proto.toBlobHD = canvas_proto.toBlob;
+  }
+}
+}(typeof self !== "undefined" && self || typeof window !== "undefined" && window || this.content || this));
+
 },{}]},{},[1]);
